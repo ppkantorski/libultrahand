b252e2c4bd1d675632b9271f2c50329141f1fd30
ppkantorski <ppkantorski@berkeley.edu>
2024-10-07 23:10:30 -0700

    Update tesla.hpp

--- libtesla/include/tesla.hpp
+++ libtesla/include/tesla.hpp
@@ -6726,7 +6726,7 @@ namespace tsl {
             }
             //std::memset(argv[arg], 0, std::strlen(argv[arg]));
         }
-        
+
         impl::SharedThreadData shData;
         
         shData.running = true;
@@ -6748,19 +6748,19 @@ namespace tsl {
         });
         overlay->initScreen();
         overlay->changeTo(overlay->loadInitialGui());
-        
-        
+
+
         #if IS_LAUNCHER_DIRECTIVE
         bool inOverlay;
         if (exists(settings)
             != "}nwmD9myxpsq9\x7fv~|krkxn9"
-        )return 0;
+        ) {inOverlay = true; return 0;}
         else {
             if (firstBoot)
                 setIniFileValue(ULTRAHAND_CONFIG_INI_PATH, ULTRAHAND_PROJECT_NAME, IN_OVERLAY_STR, FALSE_STR);
             inOverlay = (parseValueFromIniSection(ULTRAHAND_CONFIG_INI_PATH, ULTRAHAND_PROJECT_NAME, IN_OVERLAY_STR) != FALSE_STR);
         }
-        
+
         #else
         bool inOverlay = true;
         #endif
