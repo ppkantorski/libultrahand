a0c40ff54ab5da4038aa4731fa78feb1c2d931b7
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2025-06-26 02:28:36 -0700

    Update ultrahand.mk

--- ultrahand.mk
+++ ultrahand.mk
@@ -20,11 +20,8 @@ endif
 
 # Now add folder paths relative to TOPDIR (or absolute if fallback)
 SOURCES += \
-  $(ULTRA_DIR)/miniz \
   $(ULTRA_DIR)/libultra/source
 
 INCLUDES += \
-  $(ULTRA_DIR) \
-  $(ULTRA_DIR)/miniz \
   $(ULTRA_DIR)/libultra/include \
   $(ULTRA_DIR)/libtesla/include
