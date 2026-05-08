380663ecb586707ce59571779c75e9748fa8ab40
rashevskyv <rashevsky.v@gmail.com>
2026-05-08 12:55:57 +0300

    Fix hold progress bar not rendering on ToggleListItem with disabled click animation

--- libtesla/include/tesla.hpp
+++ libtesla/include/tesla.hpp
@@ -8062,7 +8062,8 @@ namespace tsl {
                         this->setState(this->m_state);
                     
                     this->m_stateChangedListener(this->m_state);
-                    this->triggerClickAnimation();
+                    if (m_flags.m_useClickAnimation)
+                        this->triggerClickAnimation();
                     
                     return Element::onClick(keys);
                 }
